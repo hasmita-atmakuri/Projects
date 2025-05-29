ecbc3a1882655d43a328e55d644e57a7580d047b
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-05-30 00:05:06 +0530

    Regenerate HTML when updating delivery option

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -24,6 +24,7 @@ export function addToCart(productId){
       matchingItem=cartItem;
     }
   });
+
   if(matchingItem){
     matchingItem.quantity++;
   }
@@ -48,6 +49,20 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem)=>{
+    if(productId === cartItem.productId){
+      matchingItem=cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId= deliveryOptionId;
 
   saveToStorage();
 }
