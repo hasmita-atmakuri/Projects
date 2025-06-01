8793acff615084b5f376f2f910e0ddaf6eeadec5
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-06-01 21:52:34 +0530

    Create oop vesrion of cart

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymetSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderPaymetSummary();
 renderOrderSummary();
