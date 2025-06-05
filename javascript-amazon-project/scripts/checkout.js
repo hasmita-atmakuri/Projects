c114f4b32f75a32a1f23036dfb15d3dd30bc62a7
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-06-05 23:55:44 +0530

    Add backend-prcatice file

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymetSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderPaymetSummary();
 renderOrderSummary();
