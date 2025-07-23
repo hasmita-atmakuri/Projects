d2dc8f14f9d5a92d3f9484847f0115b659982a0e
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-07-23 11:55:10 +0530

    Load products from the backend

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,10 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymetSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
-renderPaymetSummary();
-renderOrderSummary();
+loadProducts(() => {
+  renderPaymetSummary();
+  renderOrderSummary();
+});
