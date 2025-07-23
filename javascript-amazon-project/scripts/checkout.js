1528f7acf7eb0d2e6416aa18dd594c6c929da0bb
hasmita-atmakuri <atmakurihasmita@gmail.com>
2025-07-23 12:58:43 +0530

    Learn promises and convert code to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,10 +1,52 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymetSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
+
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(() => {
-  renderPaymetSummary();
+Promise.all([
+  new Promise((resolve)=> {
+    loadProducts(()=>{
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value2');
+    });
+  })
+
+]).then((values)=>{
+  console.log(values);
   renderOrderSummary();
+  renderPaymetSummary();
 });
+/*
+new Promise((resolve)=> {
+  loadProducts(()=>{
+    resolve('value1');
+  });
+
+}).then((value)=>{
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+
+}).then(()=>{
+  renderOrderSummary();
+  renderPaymetSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderPaymetSummary();
+    renderOrderSummary();
+  });
+});
+*/
